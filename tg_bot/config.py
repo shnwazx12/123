5502fd62d70dcd9b271b8c725933a684f9c83183
shnwazx12 <shnwazx12@gmail.com>
2026-04-19 21:21:50 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -10,12 +10,12 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "8253824369:AAHW-bquAM9sKP2E-88ViOnJ9FRlVfUIAcc"
+    OWNER_ID = "8566475119"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "Devoloperdev"
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = 'mongodb+srv://shnwazdeveloperx:shnwazdev@shnwazdev.sqmwbgl.mongodb.net/?appName=shnwazdev'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
     LOAD = []
     NO_LOAD = ['translation', 'rss']
